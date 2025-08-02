98c1f9dbbc38ec507ecc78a3e66accc3e946fa06
Sadiqul Islam Shakib <sadiqul.islam.shakib21@gmail.com>
2025-08-02 23:49:59 +0600

    added meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,12 +9,67 @@ import TopLoadingBar from "@/components/global-ui/ui-component/TopLoadingBar";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://meeting-sync-beta.vercel.app/"),
   title: {
-    template: '%s | MetingSync',
-    default: 'MetingSync',
+    default: "MetingSync",
+    template: "%s | MetingSync",
   },
-  description: "Schedule and manage your meetings efficiently",
-  // metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+  description: "Meeting Sync is an intelligent meeting scheduling and video conferencing platform that implements real-time WebRTC communication, automated cron-based lifecycle management, and AI-powered analytics using TF-IDF and k-means clustering. The platform provides end-to-end meeting management from slot creation through video conferencing to engagement analytics.",
+
+  applicationName: "MetingSync",
+  keywords: ["meetings", "scheduling", "calendar", "team management", "productivity", "Next.js", 'Sadiqul Islam Shakib', 'Custom TF-IDF & Clustering', 'NextAuth.js', 'WebRTC + Socket.IO', '	Fuse.js', 'Redis', 'Redux Toolkit', 'Tailwind CSS', 'TypeScript'],
+  authors: [{ name: "Sadiqul Islam Shakib", url: "https://meeting-sync-beta.vercel.app/" }],
+  creator: "Sadiqul Islam Shakib",
+  publisher: "MetingSync",
+
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: "https://meeting-sync-beta.vercel.app/",
+    title: "MetingSync | Smart Meeting Scheduler",
+    description: "Smart scheduling, real-time updates, and seamless team meetings.",
+    siteName: "MetingSync",
+    images: [
+      {
+        url: "/images/meeting-sync-1.png", // Your Open Graph banner image
+        width: 1200,
+        height: 630,
+        alt: "MetingSync - Smart Meeting Scheduler",
+      },
+    ],
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "MetingSync | Smart Meeting Scheduler",
+    description: "Easily schedule and manage your meetings with MetingSync.",
+    // creator: "@metingsync", // if you have a Twitter handle
+    images: ["https://yourdomain.com/twitter-card.png"],
+  },
+
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon-16x16.png",
+    apple: "/apple-touch-icon.png",
+    other: {
+      rel: "mask-icon",
+      url: "/safari-pinned-tab.svg",
+      color: "#5bbad5",
+    },
+  },
+
+  viewport: {
+    width: "device-width",
+    initialScale: 1,
+    maximumScale: 1,
+    userScalable: false,
+  },
+
+  alternates: {
+    canonical: "https://meeting-sync-beta.vercel.app/",
+  },
+
+  category: "video conferencing and scheduling",
 };
 
 export default function RootLayout({
